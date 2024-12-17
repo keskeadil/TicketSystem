ad7179e6a2c563fb3e6599be12700c3330977784
keske <keske@KESPER>
2024-12-16 20:05:40 -0600

    Add project files.

---
+++ TicketSystem/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HelloWorld
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("\n\t\t\t ----------------------------------");
+			Console.WriteLine("\n\t\t\t\t BOX OFFICE");
+			Console.WriteLine("\n\t\t\t ----------------------------------");
+			Console.WriteLine("\n\t\t\t\t Welcome Customer!");
+			Console.WriteLine("\n\n\t\t\t\t <1> Movie Timings");
+
+			Console.WriteLine("\n\t\t\t\t <2> Contact Us");
+			Console.WriteLine("\n\t\t\t\t <3> DTCard Registration");
+			Console.WriteLine("\n\t\t\t\t <4> Exit \n\n");
+			Console.WriteLine("\\t\\t\\t\\tEnter Your Choice :\"<<\"\\t");
+
+			int choooosee = Convert.ToInt32(Console.ReadLine()); ;
+
+			while (choooosee < 1 || choooosee > 4)
+			{
+				Console.WriteLine("Invalid selection - Please input 1 to 4 only.\n");
+				Console.Clear();
+			}
+		}
+	}
+}
